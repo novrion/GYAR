98808a0609df653c36e55b2a11f4106dd3b973a8
novrion <128396601+novrion@users.noreply.github.com>
2023-11-03 09:51:22 +0100

    The REAL one
    The REAL one

--- .old/1/.compile.ps1
+++
@@ -1,9 +0,0 @@
-g++ -O2 -o run_ar_base .\GA_ar_bot\ar_base\main.cpp .\GA_ar_bot\ar_base\moves.cpp .\GA_ar_bot\ar_base\board.cpp .\GA_ar_bot\ar_base\search.cpp
-g++ -O2 -o run_ar_alpha_beta .\GA_ar_bot\ar_alpha_beta\main.cpp .\GA_ar_bot\ar_alpha_beta\moves.cpp .\GA_ar_bot\ar_alpha_beta\board.cpp .\GA_ar_bot\ar_alpha_beta\search.cpp
-g++ -O2 -o run_ar_move_order .\GA_ar_bot\ar_move_order\main.cpp .\GA_ar_bot\ar_move_order\moves.cpp .\GA_ar_bot\ar_move_order\board.cpp .\GA_ar_bot\ar_move_order\search.cpp
-g++ -O2 -o run_ar_undo_move .\GA_ar_bot\ar_undo_move\main.cpp .\GA_ar_bot\ar_undo_move\moves.cpp .\GA_ar_bot\ar_undo_move\board.cpp .\GA_ar_bot\ar_undo_move\search.cpp
-
-g++ -O2 -o run_bb_base .\GA_bb_bot\bb_base\main.cpp .\GA_bb_bot\bb_base\moves.cpp .\GA_bb_bot\bb_base\board.cpp .\GA_bb_bot\bb_base\search.cpp
-g++ -O2 -o run_bb_alpha_beta .\GA_bb_bot\bb_alpha_beta\main.cpp .\GA_bb_bot\bb_alpha_beta\moves.cpp .\GA_bb_bot\bb_alpha_beta\board.cpp .\GA_bb_bot\bb_alpha_beta\search.cpp
-g++ -O2 -o run_bb_move_order .\GA_bb_bot\bb_move_order\main.cpp .\GA_bb_bot\bb_move_order\moves.cpp .\GA_bb_bot\bb_move_order\board.cpp .\GA_bb_bot\bb_move_order\search.cpp
-g++ -O2 -o run_bb_undo_move .\GA_bb_bot\bb_undo_move\main.cpp .\GA_bb_bot\bb_undo_move\moves.cpp .\GA_bb_bot\bb_undo_move\board.cpp .\GA_bb_bot\bb_undo_move\search.cpp
--- bb/.compile.ps1
+++
@@ -1,6 +0,0 @@
-g++ -O2 -o bb_base .\src\main.cpp .\src\board.cpp .\src\moves.cpp .\src\search_base.cpp
-g++ -O2 -o bb_undo_move .\src\main.cpp .\src\board.cpp .\src\moves.cpp .\src\search_undo_move.cpp
-g++ -O2 -o bb_alpha_beta .\src\main.cpp .\src\board.cpp .\src\moves.cpp .\src\search_alpha_beta.cpp
-g++ -O2 -o bb_move_order .\src\main.cpp .\src\board_move_order.cpp .\src\moves.cpp .\src\search_move_order.cpp
-
-$tmp = Read-Host "Press ENTER to exit "
